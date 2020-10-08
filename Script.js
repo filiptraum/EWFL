d367d936c8f07237ec347bb87b0488c2977718d6
filiptraum <offender54@ukr.net>
2020-10-08 15:49:24 +0300

    another commit
    delete _ from words!

--- Script.js
+++ Script.js
@@ -2,7 +2,7 @@ const vocabularyBlock = document.querySelector(".vocabulary");
 
 // 07.09.2020
 let engWords07092020 = "To_communicate communication telecommunication to_transmit transmission transmitter to_receive receiver transceiver to_carry carrier to_convert conversion converter to_amplify amplification amplifier circuit circuitry to_disturb disturbance rout router to_use user to_repeat repeater to_attenuate attenuation to_distinguish distinguished distinguishable indistinguishable to_divide division subdivision frequent frequently frequency perfect perfection imperfect imperfection to_allocate allocated allocation key keying";
-let ukrWords07092020 = "Спілкуватися спілкування телекомунікації передавати спосіб_передавання передавач отримати приймач приймач-передавач нести перевізник конвертувати перетворення перетворювач для_посилення посилення підсилювач окружність схема турбувати порушення розгром маршрутизатор використовувати користувач повторювати ретранслятор послабити загасання розрізняти відмінні відрізнити нерозрізнений розділити поділ підрозділ часті часто частота ідеально досконалість недосконалий недосконалість виділити/локалізувати виділений/локалізований розподіл/локалізація ключ маніпуляції";
+let ukrWords07092020 = "Спілкуватися спілкування телекомунікації передавати спосіб_передавання передавач отримати приймач приймач-передавач нести перевізник конвертувати перетворення перетворювач для_посилення посилення підсилювач ланцюга схема турбувати порушення розгром маршрутизатор використовувати користувач повторювати ретранслятор послабити загасання розрізняти відмінні відрізнити нерозрізнений розділити поділ підрозділ часті часто частота ідеально досконалість недосконалий недосконалість виділити/локалізувати виділений/локалізований розподіл/локалізація ключ маніпуляції";
 let date07092020 = "07.09.2020";
 
 function vocabulary(vocabulary, engWords, ukrWords, date) {
@@ -22,13 +22,13 @@ function vocabulary(vocabulary, engWords, ukrWords, date) {
     let theNewLeftCol = document.createElement("div");
     theNewLeftCol.classList.add("left");
     let theNewEngArr = engWords.split(" ");
-    theNewLeftCol.innerText = theNewEngArr[i];
+    theNewLeftCol.innerText = theNewEngArr[i].replace(/_/g, " ");
     theNewString.append(theNewLeftCol);
     
     let theNewRightCol = document.createElement("div");
     theNewRightCol.classList.add("right");
     let theNewUkrArr = ukrWords.split(" ");
-    theNewRightCol.innerText = theNewUkrArr[i];
+    theNewRightCol.innerText = theNewUkrArr[i].replace(/_/g, " ");
     theNewString.append(theNewRightCol);
   }  
 }
